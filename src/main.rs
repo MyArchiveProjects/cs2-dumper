a2cea1a34c6ac4e2b1651ca2ffba7a74cd52e8a0
ArchiveProjects <reverseengineercs2@gmail.com>
2025-04-02 01:22:48 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,14 @@ use std::fs::File;
 use std::path::PathBuf;
 use std::str::FromStr;
 use std::time::Instant;
+use std::io::{stdin, stdout, Write};
 
-use anyhow::Result;
-
-use clap::{ArgAction, Parser};
-
-use log::{LevelFilter, info};
-
-use memflow::prelude::v1::*;
-
+use clap::{Parser, ArgAction};
+use log::{info, LevelFilter};
 use simplelog::*;
-
+use anyhow::{Result, bail};
+use memflow::prelude::v1::*;
+use memflow_native;
 use output::Output;
 
 mod analysis;
@@ -20,43 +17,35 @@ mod output;
 mod source2;
 
 #[derive(Debug, Parser)]
-#[command(author, version)]
+#[command(author = "VacBan Team", version = "2.0", about = "CS2 Offset Dumper with Memflow")]
 struct Args {
-    /// The name of the memflow connector to use.
     #[arg(short, long)]
     connector: Option<String>,
 
-    /// Additional arguments to pass to the memflow connector.
     #[arg(short = 'a', long)]
     connector_args: Option<String>,
 
-    /// The types of files to generate.
-    #[arg(short, long, value_delimiter = ',', default_values = ["cs", "hpp", "json", "rs"])]
+    #[arg(short, long, value_delimiter = ',', default_values = ["cs"])]
     file_types: Vec<String>,
 
-    /// The number of spaces to use per indentation level.
     #[arg(short, long, default_value_t = 4)]
     indent_size: usize,
 
-    /// The output directory to write the generated files to.
     #[arg(short, long, default_value = "output")]
     output: PathBuf,
 
-    /// The name of the game process.
     #[arg(short, long, default_value = "cs2.exe")]
     process_name: String,
 
-    /// Increase logging verbosity. Can be specified multiple times.
     #[arg(short, long, action = ArgAction::Count)]
     verbose: u8,
 
-    /// Prevent creation of the cs2-dumper.log file.
     #[arg(short, long)]
     no_log_file: bool,
 }
 
 fn main() -> Result<()> {
-    let args = Args::parse();
+    let mut args = Args::parse();
 
     let level_filter = match args.verbose {
         0 => LevelFilter::Error,
@@ -73,7 +62,6 @@ fn main() -> Result<()> {
         ColorChoice::Auto,
     )];
 
-    // Create the log file by default.
     if !args.no_log_file {
         loggers.push(WriteLogger::new(
             LevelFilter::Info,
@@ -84,15 +72,31 @@ fn main() -> Result<()> {
 
     CombinedLogger::init(loggers)?;
 
+    if args.file_types == vec!["cs"] {
+        println!("\n[?] Select output format:");
+        println!("[1] cs only");
+        println!("[2] cs + json");
+        print!("> Enter 1 or 2 and press Enter: ");
+        stdout().flush()?;
+
+        let mut input = String::new();
+        stdin().read_line(&mut input)?;
+        let input = input.trim();
+
+        args.file_types = match input {
+            "2" => vec!["cs".to_string(), "json".to_string()],
+            _ => vec!["cs".to_string()],
+        };
+    }
+
     let conn_args = args
         .connector_args
-        .map(|s| ConnectorArgs::from_str(&s).expect("unable to parse connector arguments"))
+        .map(|s| ConnectorArgs::from_str(&s).unwrap_or_default())
         .unwrap_or_default();
 
     let mut os = match args.connector {
         Some(conn) => {
             let inventory = Inventory::scan();
-
             inventory
                 .builder()
                 .connector(&conn)
@@ -107,7 +111,7 @@ fn main() -> Result<()> {
             }
             #[cfg(not(windows))]
             {
-                panic!("no connector specified")
+                bail!("No connector specified and not running on Windows.")
             }
         }
     };
@@ -121,7 +125,7 @@ fn main() -> Result<()> {
 
     output.dump_all(&mut process)?;
 
-    info!("analysis completed in {:.2?}", now.elapsed());
+    info!("Analysis completed in {:.2?}", now.elapsed());
 
     Ok(())
 }
