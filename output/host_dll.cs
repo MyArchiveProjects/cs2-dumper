253f2fb429026d26f3d8d97724cc45423ebcc3be
ArchiveProjects <reverseengineercs2@gmail.com>
2025-04-30 09:07:42 -0500

    Add files via upload

---
+++ output/host_dll.cs
@@ -0,0 +1,21 @@
+// Generated using reworked version of cs2-dumper, link: https://github.com/MyArchiveProjects/cs2-dumper
+// Official repository of cs2-dumper is: https://github.com/a2x/cs2-dumper
+// 2025-04-30 14:07:04.145833 UTC
+
+namespace CS2Dumper.Schemas {
+    // Module: host.dll
+    // Class count: 2
+    // Enum count: 0
+    public static class HostDll {
+        // Parent: CAnimScriptBase
+        // Field count: 1
+        public static class EmptyTestScript {
+            public const nint m_hTest = 0x10; // CAnimScriptParam<float32>
+        }
+        // Parent: None
+        // Field count: 1
+        public static class CAnimScriptBase {
+            public const nint m_bIsValid = 0x8; // bool
+        }
+    }
+}
---
+++ output/networksystem_dll.cs
@@ -0,0 +1,16 @@
+// Generated using reworked version of cs2-dumper, link: https://github.com/MyArchiveProjects/cs2-dumper
+// Official repository of cs2-dumper is: https://github.com/a2x/cs2-dumper
+// 2025-04-30 14:07:04.145833 UTC
+
+namespace CS2Dumper.Schemas {
+    // Module: networksystem.dll
+    // Class count: 1
+    // Enum count: 0
+    public static class NetworksystemDll {
+        // Parent: None
+        // Field count: 1
+        public static class ChangeAccessorFieldPathIndex_t {
+            public const nint m_Value = 0x0; // int32
+        }
+    }
+}
