edefd425f8e584f9446bc4fc6714ce3816003c5b
Dragon_potion <96797834+Dragon2potion@users.noreply.github.com>
2025-07-13 01:46:07 +0200

    Add files via upload

---
+++ output/buttons.cs
@@ -0,0 +1,25 @@
+// Generated using reworked version of cs2-dumper, link: https://github.com/MyArchiveProjects/cs2-dumper
+// Official repository of cs2-dumper is: https://github.com/a2x/cs2-dumper
+// 2025-07-12 23:45:48.638885700 UTC
+
+namespace CS2Dumper {
+    // Module: client.dll
+    public static class Buttons {
+        public const nint attack = 0x18508E0;
+        public const nint attack2 = 0x1850970;
+        public const nint back = 0x1850BB0;
+        public const nint duck = 0x1850E80;
+        public const nint forward = 0x1850B20;
+        public const nint jump = 0x1850DF0;
+        public const nint left = 0x1850C40;
+        public const nint lookatweapon = 0x1A77030;
+        public const nint reload = 0x1850850;
+        public const nint right = 0x1850CD0;
+        public const nint showscores = 0x1A76F10;
+        public const nint sprint = 0x18507C0;
+        public const nint turnleft = 0x1850A00;
+        public const nint turnright = 0x1850A90;
+        public const nint use = 0x1850D60;
+        public const nint zoom = 0x1A76FA0;
+    }
+}
